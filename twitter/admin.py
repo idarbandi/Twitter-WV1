20e59ac7e4c03f8377d5c3d79d6f32d17aad4c3f
idarbandi <darbandidr99@gmail.com>
2023-07-16 17:28:41 +0330

    models elementary initiated

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister Groups
+admin.site.unregister(Group)
+
+
+# Mix Profile Info Into User Info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Just Display Username On Admin Pages
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+
+# Unregister Initial User
+admin.site.unregister(User)
+
+# Resgister User & Profiles
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
