d296773b8f0db736a5f81adb9b81378ffc21fe25
idarbandi <darbandidr99@gmail.com>
2023-07-18 15:26:31 +0330

    follow and unfollow backend department

--- twitter/views.py
+++ twitter/views.py
@@ -18,6 +18,21 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+
+        # Form Part
+        if request.method == "POST":
+            # GET CURRENT USER
+            current_user_profile = request.user.profile
+            # GET FORM DATA
+            action = request.POST['follow']
+            # FOLLOW OR UNFOLLOW DECISION
+            if action == 'unfollow':
+                current_user_profile.follows.remove(profile)
+            elif action == 'follow':
+                current_user_profile.follows.add(profile)
+            # SAVE THE PROFILE
+            current_user_profile.save()
+
         return render(request, 'profile.html', {'profile': profile})
     else:
         messages.success(request, ("You Must Be Logged In To View This Page"))
